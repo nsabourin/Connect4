c2ea321e381bb7e4610a1a1f38856601a552aa66
Oli <72115215+rivo3201@users.noreply.github.com>
2021-04-01 19:00:28 -0400

    Debut integration FPGA

--- Connect_Four_Console.cpp
+++ Connect_Four_Console.cpp
@@ -6,21 +6,77 @@ Creation	: 2021/02/14
 */
 
 #include <iostream>
+//#include <string>
 #include "InterfaceConsole.h"
 #include "GameMech.h"
 
+//**************************************  FPGA  *******************************************
+#include "CommunicationFPGA.h"
+//**************************************  FPGA  *******************************************
+
 int main() {
+
+	//**************************************  FPGA  *******************************************
+	BOOL statut_port = false;	//Statut du port de comm
+
+	unsigned const int nreg_lect_stat_btn	= 0;  // fpga -> PC  Statut et BTN lus FPGA -> PC
+	unsigned const int nreg_lect_swt		= 1;  // fpga -> PC  SWT lus FPGA -> PC
+	unsigned const int nreg_lect_cmpt_t		= 2;  // fpga -> PC  compteur temps FPGA -> PC
+	unsigned const int nreg_lect_can0		= 3;  // fpga -> PC  canal 0 lus FPGA -> PC
+	unsigned const int nreg_lect_can1		= 4;  // fpga -> PC  canal 1 lus FPGA -> PC
+	unsigned const int nreg_lect_can2		= 5;  // fpga -> PC  canal 2 lus FPGA -> PC
+	unsigned const int nreg_lect_can3		= 6;  // fpga -> PC  canal 3 lus FPGA -> PC
+	unsigned const int nreg_ecri_aff7sg0	= 7;  // PC	-> fpga (octet 0  aff.7 seg.)
+	unsigned const int nreg_ecri_aff7sg1		= 8;  // PC -> fpga (octet 1 aff.7 seg.)
+	unsigned const int nreg_ecri_aff7dot	= 9;  // PC -> fpga (donnees dot-points)
+	unsigned const int nreg_ecri_led		= 10; // PC -> fpga (donnees leds)
+
+	CommunicationFPGA port;	//Instance port de comm
+	if (!port.estOk()) {
+		std::cout << port.messageErreur() << std::endl;
+		return 1;
+	}
+	else {
+		std::cout << "Statut initial du port de communication : " << port.estOk() << std::endl << std::endl;
+	}
+
+	//::::::::::Exemples pour utiliser le port de comm::::::::::
+	
+	//Lire registre de l'interface
+	int statut_circuit = 0;
+	statut_port = port.lireRegistre(nreg_lect_stat_btn, statut_circuit);
+
+	std::cout << "Lecture de statut_circuit : " << (int)statut_circuit << std::endl;
+	std::cout << "Lecture de statut_port : " << statut_port << std::endl << std::endl;
+
+	//Lire un registtre des switches
+	int swt = 0;																//NOUVEAU vs l'exemple
+	if (statut_port)statut_port = port.lireRegistre(nreg_lect_swt, swt);		//lire swt
+	else { std::cout << "Erreur port nreg_lect_swt" << std::endl; exit(1); }	//exit clean tout en quittant safely
+
+	//Ecrire registre de l'interface
+	statut_port = port.ecrireRegistre(nreg_ecri_led, 1);	//envoye valeur 1 sur led(0) FPGA : signal pr passer en mode connecte
+	if (!port.estOk()) {
+		std::cout << port.messageErreur() << std::endl;
+		return 1;
+	}
+
+	//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+	system("pause");
+	system("cls");
+	//**************************************  FPGA  *******************************************
 
 	//0. Boucle rejouer
 	bool recommencer = true;
 	while (recommencer) {
 		
 		//1. Creation objets necessaires
-		interfaceConsole interface;
+		interfaceConsole interface_Objet;
 		GameMech connectF(6, 7);
 
 		//2. Afficher tableau vide & def variables
-		interface.affichageMegaUltime(connectF);
+		interface_Objet.affichageMegaUltime(connectF);
 		int	drop = 0;
 		int choix_rangee = 0;
 		bool victoire = false;
@@ -79,7 +135,7 @@ int main() {
 			else {
 				victoire = connectF.verificationMegaUltime(choix_rangee, drop);
 				system("CLS");
-				interface.affichageMegaUltime(connectF);
+				interface_Objet.affichageMegaUltime(connectF);
 			}
 
 		} while (!victoire);
